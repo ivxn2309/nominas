6ffb363a361e5fdfaccaba8e652edc115fa72ac3
Ivan Tovar <ivxn2309@gmail.com>
2015-04-14 16:34:12 -0500

    Correccion de bugs

--- src/nominas/gui/MainWindow.java
+++ src/nominas/gui/MainWindow.java
@@ -6,8 +6,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import nominas.control.DeptoController;
